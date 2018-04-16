f3ac4433d5e43e7e8aadb3b044cdc20060f93171
lekkanm <lekkanm@gmail.com>
2018-04-16 18:25:47 +0530

    Customer Edit

--- customers/customer.php
+++ customers/customer.php
@@ -11,8 +11,19 @@ Class customer{
         $query=mysqli_query($this->db_con,"select * from $customer_master_table where isactive=1");
         $rows=array();
         while($result=mysqli_fetch_assoc($query)){
-         $rows[]=$result;
+            $rows[]=$result;
          } 
+        //$result=mysqli_fetch_assoc($query);
+        return $rows;
+    }
+    
+    public function getCustomerinfo($id){
+        $customer_master_table="customer_master";
+        $query=mysqli_query($this->db_con,"select * from $customer_master_table where cus_id=$id");
+        $rows=array();
+        while($result=mysqli_fetch_assoc($query)){
+            $rows[]=$result;
+        }
         //$result=mysqli_fetch_assoc($query);
         return $rows;
     }
@@ -52,11 +63,45 @@ Class customer{
                             ";
             //echo $query;
             $res=mysqli_query($this->db_con,$query);
-            return $res;
+            if($res){
+                return $this->customerslist();
+            }else{
+                return false;
+            }
         }else{
             return false;
         }
         
+    }
+    
+    public function updateCustomer($post){
+        $customer_master_table="customer_master";
+        $status=1;
+        if($post){
+            $query="UPDATE $customer_master_table set 
+                                                cus_name='".$post['cus_name']."',
+                                                job_name='".$post['job_name']."',
+                                                mobile_no='".$post['mobile_no']."',
+                                                cur_addr='".$post['cur_addr']."',
+                                                per_addr='".$post['per_addr']."',
+                                                house='".$post['house']."',
+                                                bailee_name= '".$post['bailee_name']."',
+                                                bailee_mob= '".$post['bailee_mob']."',
+                                                security_type='".$post['security_type']."',
+                                                security_id='".$post['security_id']."',
+                                                line_id='".$post['line_id']."',
+                                                isactive= '".$status."' where cus_id='".$post['cus_id']."'";
+            //echo $query;
+            $res=mysqli_query($this->db_con,$query);
+            if($res){
+                return $this->customerslist();
+            }else{
+                return $res;
+            }
+            
+        }else{
+            return false;
+        }
     }
     
 }
--- customers/update.php
+++ customers/update.php
@@ -3,10 +3,10 @@ include '../config/db.php';
 require 'customer.php';
 
 $customerslist=new customer($connect);
-$customers_info=array();
-$customers_info=$customerslist->customerslist();
+$post=$_POST;
+$customers_info=$customerslist->updateCustomer($post);
 if($customers_info){
     print json_encode($customers_info);
 }else{
-    print json_encode($customers_info);
+    echo 0;
 }
